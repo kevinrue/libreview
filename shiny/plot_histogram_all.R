70fe86ff6b6ec6100593b07e11c6a2fb5e77781f
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2025-06-04 15:55:27 +0100

    facet histogram week vs weekend

--- shiny/plot_histogram_all.R
+++ shiny/plot_histogram_all.R
@@ -1,7 +1,14 @@
 plot_histogram_all <- function(glucose_data, config) {
   plot_data <- glucose_data %>% 
     select(`Device Timestamp`, `Historic Glucose mmol/L`) %>% 
-    filter(!is.na(`Historic Glucose mmol/L`))
+    filter(!is.na(`Historic Glucose mmol/L`)) %>% 
+    mutate(
+      weekend = factor(
+        wday(glucose_data_historic$`Device Timestamp`, label = TRUE) %in% c("Sat", "Sun"),
+        c(FALSE, TRUE),
+        c("Mon-Fri", "Sat-Sun")
+      )
+    )
   ggplot(plot_data) +
     annotate(
       geom = "rect",
@@ -11,10 +18,12 @@ plot_histogram_all <- function(glucose_data, config) {
       fill = "palegreen", alpha = 0.5) +
     geom_histogram(
       aes(`Historic Glucose mmol/L`),
+      linewidth = 0.1,
       binwidth = 0.5,
       boundary = 0,
       colour = "black", fill = "grey"
     ) +
+    facet_wrap(~weekend, nrow = 1) +
     scale_x_continuous(limits = c(0, 21), breaks = seq(0, 21, 3)) +
     labs(
       y = "Measurements"
@@ -25,3 +34,5 @@ plot_histogram_all <- function(glucose_data, config) {
     ) +
     theme(plot.margin = margin(0.5, 0.5, 0.5, 0.5, "cm"))
 }
+
+plot_histogram_all(glucose_data_historic, config)

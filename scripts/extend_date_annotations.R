c48cd89908220ee909ff80dde39b8695258d2112
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2025-06-19 14:36:26 +0100

    add helper script

---
+++ scripts/extend_date_annotations.R
@@ -0,0 +1,16 @@
+library(lubridate)
+library(readr)
+current_data <- read_csv("../data/date_annotations.csv", col_names = c("date", "type"))
+# tail(current_data)
+latest_annotated_date <- max(dmy(current_data$date))
+today <- ymd(Sys.Date())
+missing_dates <- as_date((latest_annotated_date+1):today)
+missing_dates_formatted <- format(missing_dates, format = "%d-%m-%Y")
+new_data <- bind_rows(
+  current_data,
+  tibble(
+    date = missing_dates_formatted,
+    type = "NA"
+  )
+)
+write_csv(new_data, "../data/date_annotations.csv")
